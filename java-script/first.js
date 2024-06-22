6c68f374156e55f7566849d363333daafd7e03c9
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-06-22 23:10:00 +0530

    file added

---
+++ java-script/first.js
@@ -0,0 +1,25 @@
+// console.log("welcome to web page")
+// console.log("parikshit singh")
+
+name="parikshit";
+age=24
+price=100
+console.log(name,age,price);
+
+isfollow=true
+console.log(isfollow)
+
+
+function myfunction(){
+    alert("you sucessfully login")
+}
+
+
+function myfunction1(){
+    document.getElementById("change").innerHTML="hello you sucessfully change it";
+}
+
+document.getElementById("number").innerHTML=5+2
+
+
+
