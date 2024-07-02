3c7f83b0ecd7a4df5e8c522e334d5dc21baa72ce
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-07-02 23:45:12 +0530

    file added

--- java/firstclass.java
+++ java/firstclass.java
@@ -2,14 +2,15 @@
 import java.util.*;
 
 class firstclass{
-    public static void main(String args[]){
+    @SuppressWarnings("resource")
+    public static void main(String[] args){
         //OUTPUT IN JAVA 
 
         System.out.print("hello worl");
         System.out.println("hello i am using the java");
 
         //INPUT IN JAVA 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         System.out.println("enter the name");
         String name = sc.nextLine();
         // String age = sc.nextInt();
