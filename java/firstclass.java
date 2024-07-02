460ce42be2d3aab226376bdbfe52c425e11ba72e
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-07-02 23:40:22 +0530

    file updated

--- java/firstclass.java
+++ java/firstclass.java
@@ -1,5 +1,26 @@
+
+import java.util.*;
+
 class firstclass{
     public static void main(String args[]){
-        System.out.println("hello worl");
+        //OUTPUT IN JAVA 
+
+        System.out.print("hello worl");
+        System.out.println("hello i am using the java");
+
+        //INPUT IN JAVA 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the name");
+        String name = sc.nextLine();
+        // String age = sc.nextInt();
+        System.out.println(name);
+        Scanner sr = new Scanner(System.in);
+        System.out.println("enter the age");
+        int a= sr.nextInt();
+        
+        System.out.println("the "+name+" age is "+a);
+
+
+
     }
 }
