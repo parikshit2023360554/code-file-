51f573e5059bcb2a61885033a774c4494c102fa6
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-07-18 16:32:24 +0530

    file added

---
+++ java/datatype.java
@@ -0,0 +1,17 @@
+//datatype in java 
+
+class datatype
+{
+	public static void main(String args[])
+	{
+		int a = 10;
+		char ch='t';
+		double d = 3.445;
+		System.out.println(a);
+		System.out.println("hello welcome to java");
+		System.out.println(ch);
+		System.out.println(d);
+		float f=1.233f;
+		System.out.println(f);
+	}
+}
