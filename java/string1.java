9dcb6eeac7bb3dea71310e0aa017d41f2fe75f78
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-07-21 14:45:10 +0530

    file updated

---
+++ java/string1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class string1
+{
+	public static void main(String arg[])
+	{
+		System.out.println("enter the sentance.....");
+		Scanner scan = new Scanner(System.in);
+		//String sentance = scan.next();
+		
+		// in string we use nextline();
+		//when we write only next() then it will read only one world 
+
+		//when we write nextLine() then it will read whole sentance and print it 
+
+		
+		String sentance = scan.nextLine();
+		System.out.println(sentance);
+	}
+}
+		
+		
---
+++ java/stringChatAt.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class stringChatAt
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		//comparing the string 
+
+		String aph1 = "parikshit";
+		String aph2 = "hello";
+		
+		if(aph1.compareTO(aph2)== 0)
+		{
+			System.out.println("both the sentance are equal");
+		}
+		else{
+			System.out.println("both are not equal");
+		}
+	}
+}
+			
+		
+		
