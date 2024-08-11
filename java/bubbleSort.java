3ce2d2759251470bbf1e2dd6a8965d94d4eeab86
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-08-11 14:48:06 +0530

    file added

---
+++ java/bubbleSort.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class bubbleSort 
+{
+    public static void main(String arg[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int size = scan.nextInt();
+        
+        int[] aray = new int[size];
+        System.out.println("enter the element in th array");
+        for(int i=0;i<size;i++)
+        {
+            aray[i] = scan.nextInt();
+        }
+        printArray(aray);
+
+        for(int b=0;b<aray.length-1;b++)
+        {
+            for(int j=0;j<aray.length-b-1;j++)
+            {
+                if(aray[j]>aray[j+1])
+                {
+                    int temp = aray[j];
+                    aray[j] = aray[j+1];
+                    aray[j+1] = temp;
+
+                }
+            }
+        }
+        printArray(aray);
+    }
+
+    public static void printArray(int aray[]){
+        for(int i=0;i<aray.length;i++)
+        {
+            System.out.print(aray[i]+" ");
+        }
+        System.out.println();
+    }
+}
